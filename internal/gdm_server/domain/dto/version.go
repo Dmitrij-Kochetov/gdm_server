15cd2b15c082d6ad641e0d870db0cbb802711dd5
dmitrij <cwfrcpp@gmail.com>
2023-09-08 18:38:11 +0300

    created dto for versions

--- internal/gdm_server/domain/dto/version.go
+++ internal/gdm_server/domain/dto/version.go
@@ -4,13 +4,14 @@ type Version struct {
 	ID        int    `json:"id"`
 	ProjectID int    `json:"project_id"`
 	Version   string `json:"version"`
+	Config    string `json:"config"`
 	Deleted   bool   `json:"deleted"`
 }
 
 type CreateVersion struct {
 	ProjectID int    `json:"project_id"`
 	Version   string `json:"version"`
-	Deleted   bool   `json:"deleted"`
+	Config    string `json:"config"`
 }
 
 type Versions []Version
